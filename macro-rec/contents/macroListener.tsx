38299de6fe28e96b5ef8c0480cb54c3e4057b345
CharmingDays <nawhpongmaji1@gmail.com>
2024-01-19 20:55:14 -0600

    added preload

--- macro-rec/contents/macroListener.tsx
+++ macro-rec/contents/macroListener.tsx
@@ -1,3 +1,4 @@
+import type { PlasmoCSConfig } from "plasmo";
 import { Storage } from "@plasmohq/storage";
 import {v4 as uuidv4} from "uuid";
 
@@ -5,6 +6,13 @@ import {v4 as uuidv4} from "uuid";
 const storage = new Storage();
 const storageName = "MacroRecorderKey"
 const tempStorageName = "TempMacroRecorderKey"
+
+
+
+export const config:PlasmoCSConfig = {
+    matches: ["<all_urls>"]
+}
+
 
 function createSelectorPath(elementId: string): string {
     const element = document.getElementById(elementId);
@@ -65,15 +73,67 @@ async function stepRecorder(event) {
 }
 
 async function finalizeRecordings() {
+    const recordedSteps = await storage.get(tempStorageName);
+    const currentMacros =await storage.get(storageName);
+    if (recordedSteps === "" || recordedSteps === undefined) {
+        return;
+    
+    }
 
 }
 
 
+
+function clickSelectorPaths(selectorPaths:[string]) {
+    /*
+    * Clicks all the elements in the selectorPaths array
+    */
+    for (const selector of selectorPaths) {
+        const element = document.querySelector(selector) as HTMLElement;
+        if (element) {
+            element.click();
+        }
+    }
+}
+
+
+const preloadWebpage = async () => {
+    /*
+    * Preload the webpage with the recorded steps
+    preloaded steps container example:
+    {
+        "preloads": {
+            "google.com": {
+                "uri": [selectorPath1, selectorPath2, ...]
+            }
+    }
+    */
+    const mainStorage =  await storage.get(storageName);
+    if (mainStorage.hasOwnProperty("preloads")) {
+        const preloads  = mainStorage['preloads'];
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            const currentURI = tabs[0].url;
+            const domainName = new URL(currentURI).hostname;
+            if (preloads.hasOwnProperty(domainName)) {
+                const domainSteps = preloads[domainName];
+                if (domainSteps.hasOwnProperty(currentURI)) {
+                    clickSelectorPaths(domainSteps[currentURI])
+                }
+            }
+        });
+    }
+}
+
+
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    const ButtonStates = {'Record Macro': 'Stop', 'Stop': 'Record Macro'}
     if (message.action.toLowerCase() === "record macro") {
         document.addEventListener("click", stepRecorder);
     }
     else if (message.action.toLowerCase() === "stop") {
         document.removeEventListener("click", stepRecorder);
+        finalizeRecordings();
     }
+    sendResponse({nextState: ButtonStates[message.action]});
 });
